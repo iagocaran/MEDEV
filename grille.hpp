5b3260f638e66f1432289343ab46d1a78d545eda
Iago Caran <iago.caran@gmail.com>
2021-01-07 11:57:16 +0100

    Add a description to all header files

--- grille.hpp
+++ grille.hpp
@@ -11,33 +11,31 @@
 
 #include "joueur.hpp"
 
+/**
+ * @brief Represents the game's grid
+ * Defines functions to display the grid in OpenGL, as well as to check if game ended
+ */
 class Grille {
 
 public:
-    
-    //Pour l'openGL
     static constexpr int taille = 10;
     static constexpr int width = 600;
     static constexpr int height = 600;
     static constexpr int default_x = 100;
     static constexpr int default_y = 100;
     static constexpr const char* default_name = "Bataille Navale";
-    
 private:
     //taille de la grille
     int n;
     Joueur *j1;
     Joueur *j2;
-    
     //pour l'openGL
     const char* nom_ligne[taille] = {"A","B","C","D","E","F","G","H","I","J"};
     const char* nom_colonne[taille] = {"0","1","2","3","4","5","6","7","8","9"};
-
 public:
     int getTaille() const;
     void afficherGrille();
     bool partieFinie();
-
     Grille(int _n, Joueur *_j1, Joueur *_j2);
 };
 
